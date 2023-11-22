bdf5243aef2f9c55714247f57b84b637e5a72fb0
riefive <rie.five@gmail.com>
2023-11-22 17:56:29 +0700

    chore: add check email at gql user service

---
+++ src/app/gql/user/check/route.ts
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+import useApolloClient from '@/systems/utils/apollo.client';
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const email = searchParams.get('email');
+    const emailValid =
+        email &&
+        typeof email === 'string' &&
+        new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/gi).test(email);
+    if (!email || !emailValid) {
+        return Response.json(
+            { message: 'Email is required!!!' },
+            { status: 500 }
+        );
+    }
+    try {
+        const client = useApolloClient();
+        const { data } = await client.query({
+            query: gql`
+                query {
+                    isAvailable(email: ${JSON.stringify(email)})
+                }
+            `,
+        });
+        return Response.json({ ...data, email });
+    } catch (error: any) {
+        return Response.json(
+            { message: 'Failed to get data!!!', error: error.toString() },
+            { status: 500 }
+        );
+    }
+}
